7a5698e8ed141d624cdcd2da9a1dfde6d85dd2ba
marinaAC <maru.cardozo02@gmail.com>
2021-02-23 22:14:15 -0300

    prueba segundo commit

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -19,11 +19,6 @@ function mostrar()
 
 	respuesta="si";
 
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
 
 	document.write("la suma de negativos es :"+sumaNegativos);
 }//FIN DE LA FUNCIÓN
